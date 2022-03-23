fd266cd2622622e9b01ac4f950253771c9a518ba
Lucas klafke <lucasnkz11@gmail.com>
2022-03-23 09:22:23 -0300

    Schedule created

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../home/Home"
+import schedule from "../schedule/Schedule"
 export default function App(){
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />}/>
+                <Route path="/filme" element={<schedule/>} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/components/schedule/Schedule.jsx
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+export default function Schedule(){
+    return (
+        <>
+            <span>Selecione o horário</span>
+        </>
+    )
+}
